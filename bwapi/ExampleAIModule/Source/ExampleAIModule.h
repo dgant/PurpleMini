6ac0c2e68888383929c3ef677f65dd47c2806377
N00byEdge <me@hannesbredberg.se>
2018-06-15 08:04:53 +0200

    395 bytes!

--- bwapi/ExampleAIModule/Source/ExampleAIModule.h
+++ bwapi/ExampleAIModule/Source/ExampleAIModule.h
@@ -1,4 +1,3 @@
 #include <BWAPI.h>
 #define UN using namespace
-UN BWAPI;UN Filter;UN std;using ss=string;using U=Unit;using b=bool;using i=int;using V=void;auto&g=BroodwarPtr;
-struct ExampleAIModule:AIModule{V onStart(){}V onFrame(){auto s=g->self();g->drawTextScreen(0,0,"Frame:%d",g->getFrameCount());}V onUnitShow(U u){}V onUnitHide(U u){}V onUnitCreate(U u){}V onUnitDestroy(U u){}V onUnitMorph(U u){}V onUnitRenegade(U u){}V onUnitComplete(U u){}};
+UN BWAPI;UN Filter;UN std;using U=Unit;using V=void;auto&g=BroodwarPtr;struct ExampleAIModule:AIModule{V onStart(){}V onFrame(){auto s=g->self();g->drawTextScreen(0,0,"Frame:%d",g->getFrameCount());}V onUnitShow(U u){}V onUnitHide(U u){}V onUnitCreate(U u){}V onUnitDestroy(U u){}V onUnitMorph(U u){}V onUnitRenegade(U u){}V onUnitComplete(U u){}};
