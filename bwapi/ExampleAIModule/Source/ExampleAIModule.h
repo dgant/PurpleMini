83a11e6698314e563688db21f230de617dadc000
dgant <dgant@users.noreply.github.com>
2018-10-29 21:22:09 -0400

    Training SCVs and fighting

--- bwapi/ExampleAIModule/Source/ExampleAIModule.h
+++ bwapi/ExampleAIModule/Source/ExampleAIModule.h
@@ -2,7 +2,10 @@
 #define A auto
 #define G using
 #define N G namespace
+#define C goto q;
 N BWAPI;
+N UnitTypes;
+N Filter;
 N std;
 G U=UnitInterface*;
 G V=void;
@@ -12,9 +15,22 @@ struct ExampleAIModule:AIModule{
 V onFrame(){
   A*s=g->self();
   for(U u:g->getAllUnits()) {
-    int f=u->getPlayer()==s;
     A t=u->getType();
-    (m=m&&m->exists()?m:t.isMineralField()?u:0)&&f&&u->isGatheringMinerals()||u->gather(m);
+    int
+    iF=u->getPlayer()==s,
+    iW=t.isWorker(),
+    iC=t.isResourceDepot(),
+    ag=t.maxGroundHits(),
+    aa=t.maxAirHits();
+    if(iF){
+      if(iC){u->train(Terran_SCV);C}
+      if(U v=u->getClosestUnit(IsEnemy,256)){
+        iW||u->getGroundWeaponCooldown()<3?u->getTarget()!=v?u->attack(v):0:0;
+        C
+      }      
+    }
+    (m=m&&m->exists()?m:t.isMineralField()?u:0)&&iW&&iF&&u->isGatheringMinerals()||u->gather(m);
+    q:;
   }
 }
 };
