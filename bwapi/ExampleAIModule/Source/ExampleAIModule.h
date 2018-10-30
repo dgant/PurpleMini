8c469da5ea01df467982833b33b40495b5c3ad05
dgant <dgant@users.noreply.github.com>
2018-10-29 20:30:13 -0400

    Gathering minerals

--- bwapi/ExampleAIModule/Source/ExampleAIModule.h
+++ bwapi/ExampleAIModule/Source/ExampleAIModule.h
@@ -1,20 +1,22 @@
 #include <BWAPI.h>
-using namespace BWAPI;
-using namespace Filter;
-using namespace std;
-using U=Unit;
-using V=void;
-auto&g=BroodwarPtr;
-struct ExampleAIModule:AIModule{
-	V onStart(){}
-	V onFrame(){
-		
-	}
-	V onUnitShow(U u){}
-	V onUnitHide(U u){}
-	V onUnitCreate(U u){}
-	V onUnitDestroy(U u){}
-	V onUnitMorph(U u){}
-	V onUnitRenegade(U u){}
-	V onUnitComplete(U u){}
+#define A auto
+#define G using
+#define N G namespace
+N BWAPI;
+N std;
+G U=UnitInterface*;
+G V=void;
+A&g=BroodwarPtr;
+U m=0;
+class ExampleAIModule:AIModule{
+V onFrame(){
+  A*s=g->self();
+  m=m&&m->exists()?m:0;
+  for(U u:g->getAllUnits()) {
+    int f=u->getPlayer()==s;
+    A t=u->getType();
+    m=m?m:t.isMineralField()?u:0;
+    m&&f&&!u->isGatheringMinerals()&&u->gather(m);
+  }
+}
 };
